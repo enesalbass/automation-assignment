232b132234a88019a6a9873b787748121f00f9bd
enesalbass <albasenes35@gmail.com>
2026-02-25 12:27:50 +0300

    Add DriverFactory and BaseTest

---
+++ src/test/java/core/base/BaseTest.java
@@ -0,0 +1,25 @@
+package core.base;
+
+import core.config.ConfigReader;
+import core.driver.DriverFactory;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+
+public abstract class BaseTest {
+
+    protected WebDriver driver;
+
+    @BeforeEach
+    void setUp() {
+        ConfigReader.init();
+        driver = DriverFactory.getDriver();
+        driver.manage().deleteAllCookies();
+        driver.get(ConfigReader.get("baseUrl"));
+    }
+
+    @AfterEach
+    void tearDown() {
+        DriverFactory.quitDriver();
+    }
+}
---
+++ src/test/java/core/driver/DriverFactory.java
@@ -0,0 +1,41 @@
+package core.driver;
+
+import core.config.ConfigReader;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class DriverFactory {
+
+    private static WebDriver driver;
+
+    public static WebDriver getDriver() {
+
+        if (driver == null) {
+
+            String browser = ConfigReader.getOrDefault("browser", "chrome");
+            boolean headless = ConfigReader.getBool("headless");
+
+            if (browser.equalsIgnoreCase("chrome")) {
+                WebDriverManager.chromedriver().setup();
+
+                ChromeOptions options = new ChromeOptions();
+                if (headless) {
+                    options.addArguments("--headless=new");
+                }
+
+                driver = new ChromeDriver(options);
+            }
+        }
+
+        return driver;
+    }
+
+    public static void quitDriver() {
+        if (driver != null) {
+            driver.quit();
+            driver = null;
+        }
+    }
+}
